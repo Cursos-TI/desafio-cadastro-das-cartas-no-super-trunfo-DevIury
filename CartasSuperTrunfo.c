a2323cc5acaf8b852c8a7ab48423d7785c20c675
Iury Gabriel <168091111+DevIury@users.noreply.github.com>
2025-01-28 12:36:08 +0000

    ALterei o printf sobre os pontos turisticos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -21,18 +21,16 @@ int main() {
 
     printf("Digite a área: \n");
     scanf("%f", &area);
+    
+    printf("\nCite quantos pontos turísticos a cidade possui: \n");
+    scanf("%d ", &pontos);
 
-    printf("Cite quantos pontos turísticos a cidade possui: \n");
-    scanf("%d", &pontos);
+    printf("");
 
-
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    return 0;
+   
 }
